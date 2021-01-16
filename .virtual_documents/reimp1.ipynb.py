7b3e1447235c0281a64a3fd79051e6eaf1c1d4d1
Andrei <inwaves@live.com>
2021-01-16 18:54:17 +0200

    Dataset ready, but input mismatch

--- .virtual_documents/reimp1.ipynb.py
+++ .virtual_documents/reimp1.ipynb.py
@@ -32,7 +32,7 @@ stride = 3
 sentences = []
 next_chars = []
 
-for i in range(len(text)-maxlen):
+for i in range(0, len(text)-maxlen, stride):
     sentences.append(text[i:i+maxlen])
     next_chars.append(text[i+maxlen])
 
@@ -40,12 +40,30 @@ for i in range(len(text)-maxlen):
 print("Sentence: {}\nNext character: {}".format(sentences[25], next_chars[25]))
 
 
-    
-x = np.zeros(((len(sentences), maxlen, len(unique_chars))))
-
+x = np.zeros((len(sentences), maxlen, len(unique_chars)))
 y = np.zeros((len(sentences), len(unique_chars)))
 
-shape_of_examples = None # placeholder—I need to find out what my inputs look like
+# Let's now go through our sentences and characters and encode examples.
+for sentence_index, sentence in enumerate(sentences):
+    for char_index, char in enumerate(sentence):
+        x[sentence_index, char_index, char_to_idx[char]] = 1
+    y[sentence_index, char_to_idx[next_chars[sentence_index]]] = 1
+
+
+print("One input char: {}".format(x[0][0]))
+print("One output char: {}".format(y[0]))
+
+
+which_sentence = 0
+chars = []
+for char_vector in x[which_sentence]:
+    chars.append(unique_chars[np.argmax(char_vector)])
+    
+print("Input sentence: {}".format("".join(chars)))
+print("Next char: {}".format(unique_chars[np.argmax(y[which_sentence])]))
+
+
+print(x.shape)
 
 
 batch_size = 128
@@ -54,16 +72,19 @@ batch_size = 128
 model = keras.Sequential(
 [
     # FIXME: what's the dimension of this input supposed to be?
-    keras.layers.Input(shape_of_examples, batch_size), 
-    keras.layers.LSTM(128),
-    keras.layers.Dense(len(unique_chars), activation="softmax")
+    keras.layers.Input((x.shape[0], x.shape[1]), batch_size, name="Input"), 
+    keras.layers.LSTM(128, name="LSTM"),
+    keras.layers.Dense(len(unique_chars), activation="softmax", name="Dense")
 ])
 
 
 optimizer = keras.optimizers.Adam()
 
 # what should the loss be? what is each loss good for?
-model.compile(loss="")
+model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+
+
+model.fit(x, y, epochs=1)
 
 
 
